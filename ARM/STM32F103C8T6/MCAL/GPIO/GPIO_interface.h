489ed6093f0bdfd8e24c9faefc10ce50b74170bf
MOHAB\mohab <mohab.ahmed.2032@gmail.com>
2021-02-03 23:38:24 +0200

    Adding the files for GPIO driver on stm32f103c8t6

---
+++ ARM/STM32F103C8T6/MCAL/GPIO/GPIO_interface.h
@@ -0,0 +1,67 @@
+/*
+ * GPIO_interface.h
+ *
+ *  Created on: Feb 3, 2021
+ *      Author: mohab
+ */
+
+#ifndef GPIO_INTERFACE_H_
+#define GPIO_INTERFACE_H_
+
+/* GPIO PORT MACROs */
+#define GPIO_U8_PORTA 				0
+#define GPIO_U8_PORTB 				1
+#define GPIO_U8_PORTC 				2
+
+/* GPIO PINS MACROs */
+#define GPIO_U8_PIN0 				0
+#define GPIO_U8_PIN1 				1
+#define GPIO_U8_PIN2 				2
+#define GPIO_U8_PIN3 				3
+#define GPIO_U8_PIN4 				4
+#define GPIO_U8_PIN5 				5
+#define GPIO_U8_PIN6 				6
+#define GPIO_U8_PIN7 				7
+#define GPIO_U8_PIN8 				8
+#define GPIO_U8_PIN9 				9
+#define GPIO_U8_PIN10 				10
+#define GPIO_U8_PIN11 				11
+#define GPIO_U8_PIN12 				12
+#define GPIO_U8_PIN13 				13
+#define GPIO_U8_PIN14 				14
+#define GPIO_U8_PIN15 				15
+
+/* GPIO INPUT options */
+#define GPIO_INPUT_ANALOG 			0b0000
+#define GPIO_INPUT_FLOATING 		0b0100
+#define GPIO_INPUT_PULL_UP_DOWN 	0b1000
+
+/* GPIO OUTPUT 2 MHz options */
+#define GPIO_OUTPUT_2MHZ_PP			0b0010
+#define GPIO_OUTPUT_2MHZ_OD			0b0110
+#define GPIO_OUTPUT_2MHZ_AF_PP 		0b1010
+#define GPIO_OUTPUT_2MHZ_AF_OD 		0b1110
+
+/* GPIO OUTPUT 10 MHz options */
+#define GPIO_OUTPUT_10MHZ_PP		0b0001
+#define GPIO_OUTPUT_10MHZ_OD		0b0101
+#define GPIO_OUTPUT_10MHZ_AF_PP 	0b1001
+#define GPIO_OUTPUT_10MHZ_AF_OD 	0b1101
+
+/* GPIO OUTPUT 50 MHz options */
+#define GPIO_OUTPUT_50MHZ_PP		0b0011
+#define GPIO_OUTPUT_50MHZ_OD		0b0111
+#define GPIO_OUTPUT_50MHZ_AF_PP 	0b1011
+#define GPIO_OUTPUT_50MHZ_AF_OD 	0b1111
+
+/* GPIO PIN value */
+#define GPIO_U8_HIGH				1
+#define GPIO_U8_LOW					0
+
+void GPIO_voidSetPinDirection(u8 Copy_u8PortID, u8 Copy_u8PinID, u8 Copy_u8Mode);
+void GPIO_voidSetPinValue(u8 Copy_u8PortID, u8 Copy_u8PinID, u8 Copy_u8Value);
+u8 GPIO_u8GetPinValue(u8 Copy_u8PortID, u8 Copy_u8PinID);
+
+/* SET PORT DIRECTION */
+/* SET PORT VALUE */
+#endif /* GPIO_INTERFACE_H_ */
---
+++ ARM/STM32F103C8T6/MCAL/GPIO/GPIO_program.c
@@ -0,0 +1,133 @@
+/*
+ * GPIO_program.c
+ *
+ *  Created on: Feb 3, 2021
+ *      Author: mohab
+ */
+
+#include "STDTYPES.h"
+#include "BIT_MATH.h"
+
+#include "GPIO_private.h"
+#include "GPIO_config.h"
+#include "GPIO_interface.h"
+
+
+void GPIO_voidSetPinDirection(u8 Copy_u8PortID, u8 Copy_u8PinID, u8 Copy_u8Mode)
+{
+	/* Range Check */
+	if ( (Copy_u8PortID<3) && (Copy_u8PinID<16) )
+	{
+		switch (Copy_u8PortID)
+		{
+		case GPIO_U8_PORTA:
+			/* LOW PORT */
+			if (Copy_u8PinID<8)
+			{
+				/* Bit masking means clear the bit/s then assign them */
+				GPIOA_CRL &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOA_CRL |=( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+			/* HIGH PORT */
+			else
+			{
+				Copy_u8PinID-=8;
+				GPIOA_CRH &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOA_CRH |= ( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+		break;
+		case GPIO_U8_PORTB:
+			/* LOW PORT */
+			if (Copy_u8PinID<8)
+			{
+				/* Bit masking means clear the bit/s then assign them */
+				GPIOB_CRL &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOB_CRL |=( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+			/* HIGH PORT */
+			else
+			{
+				Copy_u8PinID-=8;
+				GPIOB_CRH &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOB_CRH |= ( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+		break;
+		case GPIO_U8_PORTC:
+			/* LOW PORT */
+			if (Copy_u8PinID<8)
+			{
+				/* Bit masking means clear the bit/s then assign them */
+				GPIOC_CRL &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOC_CRL |= ( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+			/* HIGH PORT */
+			else
+			{
+				Copy_u8PinID-=8;
+				GPIOC_CRH &=~( ((u32)0b1111)<<(Copy_u8PinID*4) );
+				GPIOC_CRH |= ( ((u32)Copy_u8Mode)<<(Copy_u8PinID*4) );
+			}
+		break;
+		}
+	}
+	else
+	{
+		/* Error */
+	}
+}
+
+void GPIO_voidSetPinValue(u8 Copy_u8PortID, u8 Copy_u8PinID, u8 Copy_u8Value)
+{
+	/* Range Check */
+	if ( (Copy_u8PortID<3) && (Copy_u8PinID<16) )
+	{
+		switch (Copy_u8PortID)
+		{
+			case GPIO_U8_PORTA:
+				switch (Copy_u8Value)
+				{
+					case GPIO_U8_HIGH:	SET_BIT(GPIOA_ODR,Copy_u8PinID);	break;
+					case GPIO_U8_LOW:	CLR_BIT(GPIOA_ODR,Copy_u8PinID);	break;
+					default: /* Error */ break;
+				}
+			break;
+			case GPIO_U8_PORTB:
+				switch (Copy_u8Value)
+				{
+					case GPIO_U8_HIGH:	SET_BIT(GPIOB_ODR,Copy_u8PinID);	break;
+					case GPIO_U8_LOW:	CLR_BIT(GPIOB_ODR,Copy_u8PinID);	break;
+					default: /* Error */ break;
+				}
+			break;
+			case GPIO_U8_PORTC:
+				switch (Copy_u8Value)
+				{
+					case GPIO_U8_HIGH:	SET_BIT(GPIOC_ODR,Copy_u8PinID);	break;
+					case GPIO_U8_LOW:	CLR_BIT(GPIOC_ODR,Copy_u8PinID);	break;
+					default: /* Error */ break;
+				}
+			break;
+		}
+	}
+}
+
+u8 GPIO_u8GetPinValue(u8 Copy_u8PortID, u8 Copy_u8PinID)
+{
+	/* Local value to return the answer and init it to anything but ur answers */
+	u8 Local_u8Value=5;
+	/* Range Check */
+	if ( (Copy_u8PortID<3) && (Copy_u8PinID<16) )
+	{
+		switch (Copy_u8PortID)
+		{
+			case GPIO_U8_PORTA:	Local_u8Value=GET_BIT(GPIOA_IDR,Copy_u8PinID);	break;
+			case GPIO_U8_PORTB:	Local_u8Value=GET_BIT(GPIOB_IDR,Copy_u8PinID);	break;
+			case GPIO_U8_PORTC:	Local_u8Value=GET_BIT(GPIOC_IDR,Copy_u8PinID);	break;
+		}
+	}
+	else
+	{
+		/* Error */
+	}
+	return Local_u8Value;
+}
